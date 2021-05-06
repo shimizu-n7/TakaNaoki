c2e02acab7921bc6af07adc153ed2abc912d2764
shimizu-n7 <nagashimi11n@gmail.com>
2021-05-06 12:20:43 +0900

    NewStage
    シーン作成
    コイン生成
    ボタンでシーンを切り替え

--- Assets/Script/Coin.cs
+++ Assets/Script/Coin.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class  Coin  : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,6 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(new Vector3(0, 0, 90) * Time.deltaTime); 
     }
 }
